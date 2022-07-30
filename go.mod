f994cb1222189574f0c6dc7a6c19d0ca142b566c
QGB <qgbcs1@gmail.com>
2022-07-31 00:43:13 +0800

    gihs

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/QGB/heroku-golang
 
 go 1.17
+
+require github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510
--- main.go
+++ main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
 	"os"
+	"bytes"
+	"flag"
+	"fmt"
+	"github.com/google/shlex" //go get github.com/google/shlex 
+	"net/http"
+	"os/exec"
 )
 
-func main() {
-	http.HandleFunc("/", helloWorld)
-	if err := http.ListenAndServe(":"+os.Getenv("PORT"), nil); err != nil {
-		panic(err)
+func eval_cmd(w http.ResponseWriter, q *http.Request) {
+	app := q.URL.Path[1:]
+	tokens, _ := shlex.Split(app)
+	fmt.Fprintf(w, "%#v\n", tokens)
+	cmd := exec.Command(tokens[0], tokens[1:]...)
+
+	var outb, errb bytes.Buffer
+	cmd.Stdout = &outb
+	cmd.Stderr = &errb
+	err := cmd.Run()
+	if err != nil {
+		fmt.Fprintf(w, "%#v\n", err)
+	}
+	fmt.Fprintf(w, errb.String()+"\n"+outb.String()+"\n")
+
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	for name, headers := range req.Header {
+		for _, h := range headers {
+			fmt.Fprintf(w, "%v: %v\n", name, h)
+		}
 	}
 }
 
-func helloWorld(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello Full Stack Developers Crazzy Crazzyy Crazzyy</h1>")
+func main() {
+	// listen := flag.String("l", ":3000", "http listen [address]:port")
+	listen := ":"+os.Getenv("PORT")
+	flag.Parse()
+
+	http.HandleFunc("/", eval_cmd)
+	http.HandleFunc("/headers", headers)
+
+	fmt.Println("HTTP Server eval_cmd listening at", listen)
+	if err := http.ListenAndServe(listen, nil); err != nil {
+		panic(err) // failure/timeout shutting down the server gracefully
+	}
+
 }
