597b95f76fdceeca0d648e50664f77d60cee5ab1
QGB <qgbcs1@gmail.com>
2022-07-31 00:51:18 +0800

    start.py

--- start.py
+++ start.py
@@ -1,9 +1,10 @@
 #coding=
 import os
-
 PORT=os.environ.get('PORT', '5000')
 
 
+import http.server
+import socketserver
 Handler = http.server.SimpleHTTPRequestHandler
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print("serving at port", PORT)
