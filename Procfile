0b81c9b9e2ed0efeab6615db87f154872237e330
QGB <qgbcs1@gmail.com>
2022-07-31 01:08:06 +0800

    ttyd.x86_64

--- Procfile
+++ Procfile
@@ -1,1 +1,3 @@
 web: python start.py
+
+#web: ./bin/heroku-golang 
--- start.py
+++ start.py
@@ -1,11 +1,14 @@
-#coding=
+#coding=utf-8
 import os
 PORT=os.environ.get('PORT', '5000')
 
+def http_server():
+    import http.server
+    import socketserver
+    Handler = http.server.SimpleHTTPRequestHandler
+    with socketserver.TCPServer(("", int(PORT)), Handler) as httpd:
+        print("serving at port", PORT)
+        httpd.serve_forever()
 
-import http.server
-import socketserver
-Handler = http.server.SimpleHTTPRequestHandler
-with socketserver.TCPServer(("", int(PORT)), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+
+os.system('ttyd.x86_64 -p ' + PORT + ' bash')        
